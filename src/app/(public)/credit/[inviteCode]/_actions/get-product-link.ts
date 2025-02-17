3c2bf1f3b939037bc34636e8b3f0868f8796b816
Khoa Tran <khoa.trandang30620@gmail.com>
2025-02-17 09:37:44 +0700

    update new link

--- src/app/(public)/credit/[inviteCode]/_actions/get-product-link.ts
+++ src/app/(public)/credit/[inviteCode]/_actions/get-product-link.ts
@@ -34,27 +34,27 @@ const getProductLink = async (
 
 	let link: string
 
-	// if (productId.startsWith('vpb') && productId !== 'vpbankneo') {
-	// 	const prefillInfo = enc
-	// 		.encode(
-	// 			JSON.stringify({
-	// 				name: values.fullname,
-	// 				email: values.email,
-	// 				phone: values.phone
-	// 			})
-	// 		)
-	// 		.replace(/\+/g, '-')
-	// 		.replace(/\//g, '_')
-	// 		.replace(/\=/g, '.')
+	if (productId.startsWith('vpb') && productId !== 'vpbankneo') {
+		const prefillInfo = enc
+			.encode(
+				JSON.stringify({
+					name: values.fullname,
+					email: values.email,
+					phone: values.phone
+				})
+			)
+			.replace(/\+/g, '-')
+			.replace(/\//g, '_')
+			.replace(/\=/g, '.')
 
-	// 	link = `https://cards.fimi.tech/?click_id=${oid}&partner=Fimi_affiliate&affiliate_code=${info.publisherCode}&info=${prefillInfo}`
+		link = `https://cards.fimi.tech/?click_id=${oid}&partner=Fimi_affiliate&affiliate_code=${info.publisherCode}&info=${prefillInfo}`
 
-	// 	return {
-	// 		data: {
-	// 			link
-	// 		}
-	// 	}
-	// }
+		return {
+			data: {
+				link
+			}
+		}
+	}
 
 	link = product.link.replace('{{orderId}}', oid)
 
